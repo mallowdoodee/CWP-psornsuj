d751349867a13b6b38b3c7380885d378371b4e18
Ploy <ployporntip25@gmail.com>
2026-09-21 15:18:10 +0700

    commit

---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Pornthip"
+last_name = "Sorn"
+whole_name = first_name + " " + last_name
+print(whole_name)
---
+++ cell01/ex02/age.py
@@ -0,0 +1,4 @@
+current_age=22
+years = 42
+my_age = current_age + years
+print(my_age)
